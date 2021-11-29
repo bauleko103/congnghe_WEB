90179af801097df725d3a74492afa1a99b665551
Hiệp Trương <90437639+HiepTruong2505@users.noreply.github.com>
2021-11-29 10:34:12 +0700

    Add about

--- QuanLyKhachSan/src/App.js
+++ QuanLyKhachSan/src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import { BrowserRouter ,Routes, Route} from "react-router-dom";
-import {Home} from './pages/Home';
-import News from './pages/News';
-import Projects from './pages/Projects';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
-import {HeaderW} from './components/Layouts/HeaderW';
-import {FooterW} from './components/Layouts/FooterW';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import News from "./pages/News";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { HeaderW } from "./components/Layouts/HeaderW";
+import { FooterW } from "./components/Layouts/FooterW";
 
 function App() {
   return (
     <BrowserRouter>
-      <HeaderW/>
+      <HeaderW />
       <Routes>
-        <Route path='/home' element={<Home/>} />
-        <Route path='/news' element={<News/>} />
-        <Route path='/projects' element={<Projects/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />\
+        <Route path="/home" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />\
       </Routes>
-      <FooterW/>
-     </BrowserRouter>
-    
-   
+      <FooterW />
+    </BrowserRouter>
   );
 }
 
---
+++ QuanLyKhachSan/src/components/about/AboutCard.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import { Link } from "@mui/material";
+import PropTypes from "prop-types";
+
+const StyleCard = styled(Card)({
+  marginTop: 8,
+  marginBottom: 8,
+  borderRadius: 0,
+  boxShadow: "none",
+});
+
+const StyleCardContent = styled(CardContent)({
+  padding: "0",
+});
+
+const StyleCardActions = styled(CardActions)({
+  padding: "10px 0",
+});
+
+const StyleLink = styled(Link)({
+  color: "black",
+  textDecoration: "none",
+  fontWeight: "700",
+});
+export default function AboutCard({ about }) {
+  return (
+    <StyleCard sx={{ maxWidth: 250 }}>
+      <CardMedia
+        component="img"
+        height="300"
+        image={about.image}
+        alt="green iguana"
+      />
+      <StyleCardContent>
+        <StyleCardActions>
+          <StyleLink href={about.url}>{about.title}</StyleLink>
+        </StyleCardActions>
+        <Typography variant="body2" color="text.secondary">
+          {about.description}
+        </Typography>
+      </StyleCardContent>
+    </StyleCard>
+  );
+}
+
+AboutCard.propTypes = {
+  about: PropTypes.array.isRequired,
+};
