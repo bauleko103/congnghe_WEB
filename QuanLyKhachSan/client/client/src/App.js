ee3617df0abd2d9f48156259ca872fb9fe97ec89
Phongtor21 <pn02102001@gmail.com>
2021-12-01 14:10:42 +0700

    update About

--- QuanLyKhachSan/client/client/src/App.js
+++ QuanLyKhachSan/client/client/src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter ,Routes, Route} from "react-router-dom";
 import {Home} from './pages/Home';
 import News from './pages/News';
 import Projects from './pages/Projects';
-import { About } from './pages/About';
+import About from './pages/About';
 import { Contact } from './pages/Contact';
 import {HeaderW} from './components/Layouts/HeaderW';
 import {FooterW} from './components/Layouts/FooterW';
+
 
 function App() {
   return (
